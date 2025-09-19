81a4ecf21fc07aba5e4fecacc3171e9fb491b101
engr-tunde <mudashir.tunde.abdul@gmail.com>
2025-09-19 12:23:28 +0100

    madmin1

--- src/pages/members/index.jsx
+++ src/pages/members/index.jsx
@@ -5,19 +5,31 @@ import MemberTable from "../../components/members/MemberTable.jsx";
 
 function DashboardMembersPage() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-0">
       <DashboardNavBar
         title="Members"
         subtitle="Manage and search members with ease."
       />
       <div className="flex justify-end">
-        <button className="text-white bg-black px-3 py-2 cursor-pointer">Add members</button>
+        <button className="text-white bg-black px-3 py-2 cursor-pointer">
+          Add members
+        </button>
       </div>
       <div className="my-5">
         <h2 className="text-xl">Roles and their responsibilities</h2>
-        <p><span className="font-semibold">Operations Manager</span> manages order fulfilment, warehouse receipts, and customer service issues.</p>
-        <p><span className="font-semibold">Finance Manager</span> manages brand payouts, refunds, reconciliations, and financial reporting.</p>
-        <p><span className="font-semibold">Brand Manager</span> onboards and verifies brands while monitoring compliance and managing product listings</p>
+        <p>
+          <span className="font-semibold">Operations Manager</span> manages
+          order fulfilment, warehouse receipts, and customer service issues.
+        </p>
+        <p>
+          <span className="font-semibold">Finance Manager</span> manages brand
+          payouts, refunds, reconciliations, and financial reporting.
+        </p>
+        <p>
+          <span className="font-semibold">Brand Manager</span> onboards and
+          verifies brands while monitoring compliance and managing product
+          listings
+        </p>
       </div>
       <div className="w-full flex flex-col gap-8">
         <MemberTable />
