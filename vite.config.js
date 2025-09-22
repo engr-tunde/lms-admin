c64ef230631db2ddf069b0754a61c976e47b7740
Gabriel Okiri <gabrielookiri@gmail.com>
2025-09-22 13:59:23 +0100

    update the dockerfile port

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://elderintelligence.com/",
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",  // listen on all network interfaces
+    port: 5173,       // dev server port
+    allowedHosts: "all" // allow all hostnames (*)
+  }
 });
