04987defe7c5b7354891d9a78759408fe2aeb1a5
engr-tunde <mudashir.tunde.abdul@gmail.com>
2025-11-21 12:25:30 +0100

    auth handled4

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // listen on all addresses, not just localhost
-    port: 5173,      // default dev port
-    allowedHosts: [
-      "marse-admin-frontend-dev.delightfulsea-6d057c7f.centralus.azurecontainerapps.io"
-    ]
-    // or: allowedHosts: "all"  // allow any host (less secure, fine for dev)
-  }
+  base: "/",
 });
